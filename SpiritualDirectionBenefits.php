c1e208dbfbd53e21fdfa004558e1ba363bb264a1
abhardwaj2 <122378686+abhardwaj2@users.noreply.github.com>
2024-03-08 16:21:54 -0500

    added more content

---
+++ SpiritualDirectionBenefits.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Spiritual Direction Benefits</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<?php include "header.php";?>
+<img src="images/background14.jpg" alt="background image" class="background" />
+<div class="content-container">
+    <nav>
+        <ul id="navigation">
+            <li><a href="SpiritualDirectionOverview.php">Overview</a></li>
+            <li><a href="SpiritualDirectionBenefits.php" style="background-color: rgb(98, 100, 167); color: white;">Benefits</a></li>
+            <li><a href="SpiritualDirectionEndorsements.php">Endorsements</a></li>
+        </ul>
+        <hr>
+    </nav>
+    <h1 class="content-main-heading">Benefits</h1>
+    <p>The <em>Intensive Journal</em> method can serve as a valuable resource for both spiritual directors and directees. </p>
+
+    <p class="subheading">Connect With Many Facets of Your Life</p>
+    <ul>
+        <li>Develop a foundation on your life as you work in many areas to develop a more meaningful life.</li>
+        <li>Realize connections and relationships between different areas of your life and the spiritual component.</li>
+    </ul>
+
+    <p class="subheading">Enhance Your Listening Skills</p>
+    <ul>
+        <li>Increase your attentiveness to recognizing many different facets of experience.</li>
+        <li>Become more receptive to hearing your inner voice and less judgmental and reactionary. </li>
+    </ul>
+
+    <p class="subheading">Obtain a Direct Experience in Your Life</p>
+    <ul>
+        <li>Benefit from a deepening workshop experience away from your daily routine to connect with your inner self.</li>
+    </ul>
+
+    <p class="subheading">Experience a Reflective and Contemplative Process</p>
+    <ul>
+        <li>Apply a neutral meditative process for becoming centered and deepening your inward journey. </li>
+    </ul>
+
+    <p class="subheading">Learn Practical Exercises for Connecting With the Spiritual Dimension</p>
+    <ul>
+        <li>Deepen your experience of the Sacred in your life.</li>
+        <li>Discern the deep inner purpose and direction of your life as it can be lived most authentically in the world.</li>
+        <li>Connect with and develop greater awareness and sensitivity to important religious/spiritual experiences.</li>
+        <li>Clarify beliefs, values, priorities truths, purpose and religious/ spiritual experiences.</li>
+    </ul>
+
+    <p class="subheading">Tap Into the Wisdom of Spiritual Figures</p>
+    <p>Use Progoff's technique of inner dialogue to connect with the wisdom of spiritual figures to obtain clarity and understanding to resolve issues</p>
+
+
+    <p class="subheading">Enhance Spiritual Direction Sessions</p>
+    <p>Both spiritual directors and directees can benefit from attending Intensive Journal workshops. </p>
+    <ul>
+        <li>Spiritual Directors: Increased Effectiveness</li>
+        <ul>
+            <li>Learn fresh processes for spiritual growth to develop and integrate life experiences.</li>
+            <li>Broaden perspectives and intuitive abilities.</li>
+        </ul>
+        <li>Directees: Greater Participation Possible</li>
+        <ul>
+            <li>Uncover and develop issues and experiences.</li>
+            <li>Integrate selected exercises into sessions.</li>
+        </ul>
+        <li>Spiritual Direction Sessions: Greater Depth </li>
+        <ul>
+            <li>Sessions can move to a deeper level.</li>
+            <li>Directees can gain insights and clarity.</li>
+        </ul>
+        <li>Post-session: Life-Long Tool for Spiritual-Growth</li>
+    </ul>
+
+</div>
+<br>
+
+
+</body>
+
+</html>
---
+++ SpiritualDirectionOverview.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Spiritual Direction Overview</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<?php include "header.php";?>
+<img src="images/background13.jpg" alt="background image" class="background" />
+<div class="content-container">
+    <nav>
+        <ul id="navigation">
+            <li><a href="SpiritualDirectionOverview.php" style="background-color: rgb(98, 100, 167); color: white;">Overview</a></li>
+            <li><a href="SpiritualDirectionBenefits.php">Benefits</a></li>
+            <li><a href="SpiritualDirectionEndorsements.php">Endorsements</a></li>
+        </ul>
+        <hr>
+    </nav>
+    <h1 class="content-main-heading">Overview</h1>
+    <p>The Progoff&trade; methodology provides both spiritual directors and directees with an integrated system of writing exercises for personal and spiritual growth. The deepening atmosphere and unique structure of the <em>Intensive Journal</em> exercises provide the opportunity for profound inner experience. </p>
+
+    <p>The <em>Intensive Journal</em> program provides a practical tool to use in spiritual direction. Our method provides spiritual directors with fresh processes for spiritual growth to develop and integrate life experiences.</p>
+
+    <p>Directees tend to become more engaged in their own development as they use our process to uncover and develop issues and experiences. Material developed through the <em>Intensive Journal</em> exercises can be integrated into the spiritual direction sessions.</p>
+
+    <p>The net result is our method can help to make the spiritual direction sessions more effective and to provide a life-long tool for ongoing spiritual growth.</p>
+
+    <p>Please have a copy of our brochure on <a href="/brochures/spiritual-direction-2019.pdf" style="color: rgb(98,100,167)">Spiritual direction ↗</a></p>
+
+</div>
+<br>
+
+
+</body>
+
+</html>
