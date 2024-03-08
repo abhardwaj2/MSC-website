e2f720ec2217844220a58db986243fa7fcfe041f
abhardwaj2 <122378686+abhardwaj2@users.noreply.github.com>
2024-03-08 15:18:59 -0500

    adding new pages and brochures

---
+++ ContemplativePracticeOverview.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Contemplative Practice Overview</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<?php include "header.php";?>
+<img src="images/background8.jpg" alt="background image" class="background" />
+<div class="content-container">
+    <nav>
+        <ul id="navigation">
+            <li><a href="ContemplativePracticeOverview.php" style="background-color: rgb(98, 100, 167); color: white;">Overview</a></li>
+            <li><a href="ContemplativePracticeBenefits.php">Benefits</a></li>
+            <li><a href="ContemplativePracticeEndorsements.php">Endorsements</a></li>
+            <li><a href="ContemplativePracticeFormofPrayer.php">Form of Prayer</a></li>
+            <li><a href="ContemplativePracticeArticles.php">Articles</a></li>
+        </ul>
+        <hr>
+    </nav>
+    <h1 class="content-main-heading">Overview</h1>
+    <p>Beginning in the early 1950s, Dr. Progoff was a leading proponent of the importance of spiritual development as an overall component of psychological healing and growth.</p>
+
+    <p>Progoff wrote one of the leading introductory commentaries and translations on The Cloud of Unknowing by the 14th century anonymous monk as he describes how individuals can become connected to God.</p>
+
+    <p>“What is separating man from God is not any physical thing but the state of his consciousness. More specifically, it is man's consciousness of his separateness, of his existence as an individual apart from God that keeps him separated from God. To overcome this awareness of separateness, he must place himself in a psychological situation that weakens, and ultimately dispenses with, the conscious guidance of his personal being. He must permit himself to drop into a condition of unconsciousness, a condition of total unknowing. This encompassing state of unknowing is the cloud of unknowing that covers him in darkness, dulls his awareness of his separate existence, and provides the medium in which his union with God may take place” - Ira Progoff</p>
+
+    <p>Dr Progoff’s approach to psychotherapy and then implemented in the Intensive Journal method stressed the importance of creating a conducive atmosphere and approaches for connecting with your true inner self. He emphasized: </p>
+
+    <p class="subheading">Direct ongoing experience</p>
+    <p>"There is no use in one person attempting to tell another person what the meaning of life is. It involves too intimate an awareness. A major part of the meaning of life is contained in the very process of discovering it. It is an ongoing experience of growth that involves a deepening of contact with reality....The meaning of life cannot be told; it has to happen to a person" 2 </p>
+
+    <p class="subheading">Silence and privacy</p>
+    <p>"Spiritual contact, the awareness of the profounder meanings of life and the experience of its symbols, is an individual endeavor that can be carried through only in the silence of privacy." 3</p>
+
+    <p class="subheading">Working at an inner level</p>
+    <p>"When our attention is focused inwardly at the depth of our being in the context of the wholeness of our life, resources for a profound knowledge of life become accessible to us." 4 </p>
+
+    <p>Please have a copy of our brochure on <a href="/brochures/spiritual-contemplative-3-2018.pdf" style="color: rgb(98, 100, 167)">Contemplative Practice ↗</a></p>
+    <hr>
+    <p>1 The Cloud of Unknowing, by Ira Progoff, PhD, page 34.</p>
+    <p>2 The Symbolic and the Real, by Ira Progoff, PhD, pages 13-14</p>
+    <p>3 At a Journal Workshop, page 5.</p>
+    <p>4 At a Journal Workshop, page 9.</p>
+
+
+</div>
+<br>
+
+
+</body>
+
+</html>
